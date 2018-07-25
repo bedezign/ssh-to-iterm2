d22c6563e3c5236f733db35cd7afc6a59024c152
Steve Guns <Steve@bedezign.com>
2018-07-25 15:02:56 +0200

    Phar creation updated: Use whitespace-stripped source and also generate a compressed variant.

--- src/create-phar.php
+++ src/create-phar.php
@@ -24,9 +24,16 @@ echo 'Adding files...', PHP_EOL;
 foreach ($files as $file) {
     /** @var SplFileInfo $file */
     $localFile = substr($file->getPathname(), \strlen($baseDir));
-    $phar->addFile(realpath($file), $localFile);
+    // Add the files without whitespace to make the regular phar as small as possible already
+    $phar->addFromString($localFile, php_strip_whitespace(realpath($file)));
     echo "'$file' => $localFile", PHP_EOL;
 }
 
-echo 'Setting stub...', PHP_EOL;
-$phar->setStub(Phar::createDefaultStub('/src/update.php'));
+echo 'Creating stub...', PHP_EOL;
+// Compressing a phar overrides the stub, forcing /index.php to be used as bootstrap.
+// So as a solution just add a dummy file that loads the actual bootstrap.
+# $phar->setStub(Phar::createDefaultStub('/src/index.php'));
+$phar->addFromString('/index.php', '<?php require __DIR__ . "/src/index.php";');
+
+echo 'Compressing...', PHP_EOL;
+$phar->compress(Phar::GZ);
